c08d0008534c97cdcf0fdb13a52684a95bc4e615
naudii <naudilogan@gmail.com>
2015-12-15 10:06:11 -0500

    Initial commit

---
+++ android/src/com/laz/map/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.laz.map.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.laz.map.MainMap;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MainMap(), config);
+    }
+}
---
+++ desktop/src/com/laz/map/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.laz.map.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.laz.map.MainMap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainMap(), config);
+	}
+}
